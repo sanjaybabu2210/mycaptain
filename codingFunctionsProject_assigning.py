35daa57e86f911c6ae2f7e48e8e00620bdefa19d
Sanjay Babu <sanjaybabu2210@gmail.com>
2020-06-14 06:17:58 +0530

    created a project file

---
+++ codingFunctionsProject_assigning.py
@@ -0,0 +1,11 @@
+lis = [3,5,6,8]
+tup = (4,5,7,8,9)
+
+lis[2] = 8     # assigning 2 index to 8
+print(tup[2])  #printing by accessing 2nd index from tuple
+print(lis)
+newdict ={'new':2,'old':3}
+
+del newdict['new']     #deleting new key and corresponding value from dict
+
+print(newdict)
